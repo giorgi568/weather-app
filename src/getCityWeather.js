fd2cb01fff0a4c1e66ea7de43cc8080f9f1a009b
giorgi568 <mateshvili02@mail.ru>
2023-07-14 23:08:51 +0400

    functions done

---
+++ src/getCityWeather.js
@@ -0,0 +1,94 @@
+async function getCurrentTemp(city) {
+  const apiKey = 'fc78ef90a1c34ac1b67144641231107';
+  const tag = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+  const response = await fetch(tag, { mode: 'cors' });
+  const currentWeather = await response.json();
+  const temp = currentWeather.current.temp_c;
+
+  console.log(currentWeather);
+  return temp;
+}
+
+async function getCurrentCondition(city) {
+  const apiKey = 'fc78ef90a1c34ac1b67144641231107';
+  const tag = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+  const response = await fetch(tag, { mode: 'cors' });
+  const currentWeather = await response.json();
+  const condition = currentWeather.current.condition.text;
+
+  console.log(condition);
+  return condition;
+}
+
+async function getCurrentTime(city) {
+  const apiKey = 'fc78ef90a1c34ac1b67144641231107';
+  const tag = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+  const response = await fetch(tag, { mode: 'cors' });
+  const currentWeather = await response.json();
+  const time = currentWeather.location.localtime;
+
+  console.log(time);
+  return time;
+}
+
+async function getCurrentWind(city) {
+  const apiKey = 'fc78ef90a1c34ac1b67144641231107';
+  const tag = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+  const response = await fetch(tag, { mode: 'cors' });
+  const currentWeather = await response.json();
+  const wind = currentWeather.current.wind_kph;
+
+  console.log(wind);
+  return wind;
+}
+
+async function getFeelsLike(city) {
+  const apiKey = 'fc78ef90a1c34ac1b67144641231107';
+  const tag = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+  const response = await fetch(tag, { mode: 'cors' });
+  const currentWeather = await response.json();
+  const fellsLike = currentWeather.current.feelslike_c;
+
+  console.log(fellsLike);
+  return fellsLike;
+}
+
+async function getHumidity(city) {
+  const apiKey = 'fc78ef90a1c34ac1b67144641231107';
+  const tag = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+  const response = await fetch(tag, { mode: 'cors' });
+  const currentWeather = await response.json();
+  const humidity = currentWeather.current.humidity;
+
+  console.log(humidity);
+  return humidity;
+}
+
+async function getMoonPhase(city) {
+  const apiKey = 'fc78ef90a1c34ac1b67144641231107';
+  const tag = `http://api.weatherapi.com/v1/astronomy.json?key=${apiKey}&q=${city}`;
+
+  const response = await fetch(tag, { mode: 'cors' });
+  const currentWeather = await response.json();
+  const moonPhase = currentWeather.astronomy.astro.moon_phase;
+
+  console.log(moonPhase);
+  return moonPhase;
+}
+
+
+export {
+  getCurrentTemp,
+  getCurrentCondition,
+  getCurrentTime,
+  getCurrentWind,
+  getFeelsLike,
+  getHumidity,
+  getMoonPhase
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import './style.css';
-console.log('heyy')
+import {
+  getCurrentTemp,
+  getCurrentCondition,
+  getCurrentTime,
+  getCurrentWind,
+  getFeelsLike,
+  getHumidity,
+  getMoonPhase,
+} from './getCityWeather';
+
+getCurrentTemp('london').catch(function (err) {
+  console.log(err);
+});
+
+getCurrentCondition('london');
+getCurrentTime('london');
+getCurrentWind('london');
+getFeelsLike('london');
+getHumidity('london');
+getMoonPhase('london')
